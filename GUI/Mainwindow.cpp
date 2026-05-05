330ec5b4d52fcb2263648695fca2438125d6a085
ashraf <mohamedashraf577@gmail.com>
2026-05-06 01:25:08 +0300

    UI files ready

--- GUI/Mainwindow.cpp
+++ GUI/Mainwindow.cpp
@@ -1,3 +1,17 @@
 //
-// Created by DELL on 4/28/2026.
+// Created by DELL on 5/6/2026.
 //
+
+// You may need to build the project (run Qt uic code generator) to get "ui_Mainwindow1.h" resolved
+
+#include "Mainwindow.h"
+#include "ui_Mainwindow1.h"
+
+
+Mainwindow1::Mainwindow1(QWidget *parent) : QWidget(parent), ui(new Ui::Mainwindow1) {
+    ui->setupUi(this);
+}
+
+Mainwindow1::~Mainwindow1() {
+    delete ui;
+}
--- GUI/Mainwindow.h
+++ GUI/Mainwindow.h
@@ -1,8 +1,32 @@
 //
-// Created by DELL on 4/28/2026.
+// Created by DELL on 5/6/2026.
 //
 
-#ifndef SNAKE_GAME_MAINWINDOW_H
-#define SNAKE_GAME_MAINWINDOW_H
+#ifndef SNAKE_GAME_MAINWINDOW1_H
+#define SNAKE_GAME_MAINWINDOW1_H
 
-#endif //SNAKE_GAME_MAINWINDOW_H
+#include <QWidget>
+
+
+QT_BEGIN_NAMESPACE
+
+namespace Ui {
+    class Mainwindow1;
+}
+
+QT_END_NAMESPACE
+
+class Mainwindow1 : public QWidget {
+    Q_OBJECT
+
+public:
+    explicit Mainwindow1(QWidget *parent = nullptr);
+
+    ~Mainwindow1() override;
+
+private:
+    Ui::Mainwindow1 *ui;
+};
+
+
+#endif //SNAKE_GAME_MAINWINDOW1_H
