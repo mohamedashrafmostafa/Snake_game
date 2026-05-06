94becc5079cec8b601baabd23808ae29b79dfc6e
ashraf <mohamedashraf577@gmail.com>
2026-05-06 16:20:46 +0300

    Icon Added

--- GUI/Mainwindow.cpp
+++ GUI/Mainwindow.cpp
@@ -1,15 +1,11 @@
-//
-// Created by DELL on 5/6/2026.
-//
-
-// You may need to build the project (run Qt uic code generator) to get "ui_Mainwindow1.h" resolved
-
 #include "Mainwindow.h"
-#include "ui_Mainwindow1.h"
-
+#include "Mainwindow.ui"
+#include <QIcon>
 
 Mainwindow1::Mainwindow1(QWidget *parent) : QWidget(parent), ui(new Ui::Mainwindow1) {
     ui->setupUi(this);
+
+    setWindowIcon(QIcon(":/snake++.png"));
 }
 
 Mainwindow1::~Mainwindow1() {
